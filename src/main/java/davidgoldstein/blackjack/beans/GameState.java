a7b5dab1fdfc0e0d85ca0169e901041ae650697e
David Goldstein <david.goldstein@target.com>
2022-03-31 15:33:35 -0500

    temp

--- src/main/java/davidgoldstein/blackjack/beans/GameState.java
+++ src/main/java/davidgoldstein/blackjack/beans/GameState.java
@@ -9,15 +9,19 @@ import java.io.Serializable;
 public class GameState implements Serializable {
     @Id
     private String id;
+    private String name;
 
     public GameState() {}
     public GameState(String id) {
         this.id = id;
     }
 
-    public String getId() {return id;}
-    public void setId(String id) {
-        this.id = id;
+    public void setName(String name ){
+        this.name = name;
     }
+    public String getName() {
+        return this.name;
+    }
+    public String getId() {return id;}
 
 }
